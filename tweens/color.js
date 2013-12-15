322c56c0f0c0aaee25d4a3a0afeabbc124e2e0fe
Jake Rosoman <jkroso@gmail.com>
2013-12-15 21:23:23 +1300

    fix color rounding

--- tweens/color.js
+++ tweens/color.js
@@ -1,20 +1,24 @@
 
-var Tween = require('string-tween')
 var parse = require('color-parser')
 
 module.exports = function(from, to){
-	return tween(rgba(from), rgba(to))
-}
-
-function toInt(n){
-	return n.toFixed(0)
+	from = rgba(from)
+	to = rgba(to)
+	var curr = to.slice()
+	return function frame(n){
+		for (var i = 0; i < 4; i++) {
+			curr[i] = (from[i] + (to[i] - from[i]) * n).toFixed(0)
+		}
+		return 'rgba(' + curr + ')'
+	}
 }
 
 function rgba(color){
 	color = parse(color)
-	return 'rgba('
-		+ color.r + ','
-		+ color.g + ','
-		+ color.b + ','
-		+ (color.a || 1) + ')'
+	return [
+		color.r,
+		color.g,
+		color.b,
+		(color.a == null ? 1 : color.a)
+	]
 }
