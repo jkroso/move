5eb354156aa3e0d2e57f4878766ea79644196d20
Jake Rosoman <jkroso@gmail.com>
2014-01-01 13:17:56 +1300

    use balance-svg-path

--- tween/path.js
+++ tween/path.js
@@ -1,18 +1,7 @@
 
-var toString = require('serialize-svg-path')
+var balance = require('balance-svg-paths')
 var tween = require('string-tween')
-var compose = require('fcomp')
-var normalize = compose(
-	require('parse-svg-path'),
-	require('abs-svg-path'),
-	require('curve-svg-path'),
-	require('rel-svg-path'))
 
 module.exports = function(from, to){
-	from = normalize(from)
-	to = normalize(to)
-	if (from.length != to.length) {
-		throw new TypeError('incompatable paths')
-	}
-	return tween(toString(from), toString(to))
+	return tween.apply(null, balance(from, to))
 }
