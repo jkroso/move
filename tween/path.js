cf865e0326f0f06b3cc03879b89484e76969c3aa
Jake Rosoman <jkroso@gmail.com>
2014-01-05 01:27:49 +1300

    add deps

--- tween/path.js
+++ tween/path.js
@@ -1,7 +1,14 @@
 
+var toString = require('serialize-svg-path')
 var balance = require('balance-svg-paths')
 var tween = require('string-tween')
+var normalize = require('fcomp')(
+  require('parse-svg-path'),
+  require('abs-svg-path'),
+  require('normalize-svg-path'),
+  require('rel-svg-path'))
 
 module.exports = function(from, to){
-  return tween.apply(null, balance(from, to))
+  var ends = balance(normalize(from), normalize(to))
+  return tween(toString(ends[0]), toString(ends[1]))
 }
