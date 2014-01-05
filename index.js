b4516af8a3002c889b2ef002a033be48d6e16704
Jake Rosoman <jkroso@gmail.com>
2014-01-05 22:56:30 +1300

    use component/query

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
+var query = require('query')
 var Move = require('./move')
 var SVG = require('./svg')
 
 module.exports = function(el){
+  if (typeof el == 'string') el = query(el)
   if (el instanceof SVGElement) return new SVG(el)
   return new Move(el)
 }
