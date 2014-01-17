ef181d14bb20018f00769f9b75566e2c97847a76
Jake Rosoman <jkroso@gmail.com>
2014-01-17 23:18:43 +1300

    add svg example

--- examples.js
+++ examples.js
@@ -5,29 +5,18 @@ var dom = require('dom')
 var css = require('css')
 
 dom('.example').each(function(el){
-	el.find('button.play').on('mousedown', function(){
-		var boxs = el.find('.box.small')
-		var boxs = [].slice.call(boxs.els)
-		var box = boxs[0]
-		eval(el.find('.source').text())
-	})
+  el.initial = el.find('.sandbox').html()
 
-	el.find('.box').each(function(box){
-		box = box.get(0)
-		box._css = {}
-		var computed = style(box)
-		for (var i = 0, len = computed.length; i < len; i++) {
-			var key = computed[i]
-			if (computed[key]) box._css[key] = computed[key]
-		}
-	})
+  el.find('button.play').on('mousedown', function(){
+    var boxs = el.find('.box.small')
+    var boxs = [].slice.call(boxs.els)
+    var box = boxs[0]
+    eval(el.find('.source').text())
+  })
 
-	el.find('h3')
-		.append('<button class="reset">↻</button>')
-		.on('mousedown', function(e){
-			el.find('.box').each(function(box){
-				box = box.get(0)
-				css(box, box._css)
-			})
-		})
+  el.find('h3')
+    .append('<button class="reset">↻</button>')
+    .on('mousedown', function(e){
+      el.find('.sandbox').html(el.initial)
+    })
 })
