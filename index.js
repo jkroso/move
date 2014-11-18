a12cd3653666ce8e5b89aebec8536a3f51206358
Jake Rosoman <jkroso@gmail.com>
2014-11-18 13:20:33 +1300

    support passing target properties to move()

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 var query = require('query')
-var Move = require('./move')
+var DOM = require('./move')
 var SVG = require('./svg')
 
-module.exports = function(el){
+module.exports = function(el, to){
   if (typeof el == 'string') el = query(el)
-  if (el instanceof SVGElement) return new SVG(el)
-  return new Move(el)
+  var move = el instanceof SVGElement ? new SVG(el) : new DOM(el)
+  for (var key in to) move.set(key, to[key])
+  return move
 }
--- test/move.test.js
+++ test/move.test.js
@@ -2,10 +2,7 @@
 var prefix = require('prefix')
 var move = require('..')
 
-var transform = prefix('transform')
-
-var box
-var mv
+var box, mv, spy
 before(function(){
   spy = chai.spy()
   box = document.createElement('div')
@@ -19,9 +16,10 @@ after(function(){
 
 describe('.set()', function(){
   it('should handle default types', function(){
-    mv.set('opacity', 0)
-      .set('margin', 5)
-      .render(.5)
+    move(box, {
+      opacity: 0,
+      margin: 5
+    }).render(.5)
     box.style.should.include({
       margin: '2.5px',
       opacity: '0.5'
@@ -41,10 +39,9 @@ describe('.then()', function(){
   })
 
   it('should pull current value down from grandparents', function(){
-    var obj = mv.x(5).then().then().x(-2).render(.5)
-    var frame = {}
-    frame[transform] = 'translateX(4px) translateY(0px) rotate(0deg) skew(0deg) scaleX(1) scaleY(1)'
-    box.style.should.include(frame)
+    move(box).x(5).then().then().x(-2).render(.5)
+    var frame = 'translateX(4px) translateY(0px) rotate(0deg) skew(0deg) scaleX(1) scaleY(1)'
+    box.style.should.have.property(prefix('transform'), frame)
   })
 
   it('should not auto-run deferred moves', function(done){
@@ -54,6 +51,6 @@ describe('.then()', function(){
     })
     var b = a.then()
     b.run = spy
-    var c = b.then()
+    b.then()
   })
 })
